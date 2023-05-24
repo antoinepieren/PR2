ac2730fd6dead565c3983a8b97de37edd3708c30
awongng <abygailwong@gmail.com>
2023-05-24 10:51:31 +0200

    c bon?

--- main.c
+++ main.c
@@ -4,61 +4,13 @@
 #pragma config OSC = INTIO67
 #pragma config PBADEN = OFF, WDT = OFF, LVP = OFF, DEBUG = ON
 
-char message[] = "Entrer vitesse: ";
-unsigned char i=0;
-unsigned char compteurBat = 0;
-unsigned char compteurSon = 0;
-unsigned char compteur = 0;
 unsigned char j;
-
-void HighISR(void);
-
-
-#pragma code HighVector=0x08
-void IntHighVector(void)
-{
-    _asm goto HighISR _endasm
-}
-#pragma code
-
-#pragma interrupt HighISR
-void HighISR(void)
-{
-    if(PIR1bits.TMR2IF) //Interruption TIMER2 + compteur
-    {
-        PIR1bits.TMR2IF=0; // Validation interruption
-        // Compteur Batterie
-        if(compteurBat == 100){
-            LATBbits.LATB5=~LATBbits.LATB5;
-            compteurBat = 0;
-        }
-        else{
-            compteurBat++;
-        }
-        // Compteur Sonar
-        if(compteurSon == 10){
-            LATBbits.LATB5=~LATBbits.LATB5;
-            compteurSon = 0;
-        }
-        else{
-            compteurSon++;
-        }
-
-    }
-
-    if(PIR1bits.TXIF && PIE1bits.TXIE)
-    {
-        PIE1bits.TXIE = 0;
-    }
-
-    if(INTCONbits.INT0IF)      //INT0
-    {
-      INTCONbits.INT0IF = 0;
-
-      //LATBbits.LATB5=~LATBbits.LATB5;
-    }
-
-}
+char messageInit[] = "Fin init";
+unsigned char marche = 0;                                       // Booléen de marche/arrêt
+//unsigned char led = 0b00000000;
+unsigned char distance;                                         // Variable distance lue par le sonar
+unsigned char mode = 0;                                         // Phase du programme
+unsigned char pwm_on = 62;                                      // Rapport cyclique pwm (250>>2)
 
 void write(char c){
     while(PIE1bits.TXIE);
